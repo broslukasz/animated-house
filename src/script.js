dc785a3aabfd022e129fc8d77fb6e6ea85bf082f
Lukasz Bros <lbros@pgs-soft.com>
2022-06-14 10:43:50 +0200

    camera farer

--- src/script.js
+++ src/script.js
@@ -257,7 +257,7 @@ window.addEventListener('dblclick', () => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 4
 camera.position.y = 2
-camera.position.z = 5
+camera.position.z = 8
 scene.add(camera)
 
 // Controls
