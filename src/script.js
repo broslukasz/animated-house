e0667340c5b23a512babfabf29e0b89d5040d194
Lukasz Bros <lbros@pgs-soft.com>
2022-06-14 10:39:43 +0200

    hide controls

--- src/script.js
+++ src/script.js
@@ -196,6 +196,7 @@ gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
+gui.close()
 scene.add(moonLight)
 
 // Door light
