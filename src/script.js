2731649927f5cce9c3dc5f5439186aa335be9c92
Lukasz Bros <lbros@pgs-soft.com>
2022-06-14 10:49:04 +0200

    camera up - revert

--- src/script.js
+++ src/script.js
@@ -256,7 +256,7 @@ window.addEventListener('dblclick', () => {
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 4
-camera.position.y = 4
+camera.position.y = 2
 camera.position.z = 8
 scene.add(camera)
 
